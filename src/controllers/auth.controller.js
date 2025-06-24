7402ac2f3361d84ea40573d97b02b9257a3b8d8c
pattaraphon1 <klgsa51@gmail.com>
2025-06-24 13:02:40 +0700

    starter with auth.route+auth.controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+export function register(req, res, next) {
+  res.json({
+    msg: 'Register controller',
+    body: req.body
+  })
+}
+
+export const login = (req, res, next) => {
+  res.json({
+    msg: 'Login controller',
+    body: req.body
+  })
+}
+
+export const getMe = (req, res, next) => {
+  res.json({
+    msg: 'Getme controller'
+  })
+}
+
