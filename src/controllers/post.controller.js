756e20b2450529e0fbd38b7898b9c5bb20ee0af8
Pattaraphon1 <klgsa51@gmail.com>
2025-06-30 15:43:25 +0700

    upload file post

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,22 @@
+export async function getAllPosts(req, res, next) {
+
+ res.json({message : "Get all posts"})
+}
+
+export async function createPost(req, res, next) {
+console.log(req.body)
+console.log(req.file)
+ res.json({message: "Create post",
+    file : req.file
+ })
+}
+
+export async function updatePost(req, res, next) {
+
+ res.json({message: "Update Post"})
+}
+
+export async function deletePost(req, res, next) {
+
+ res.json({message: "Delete Post"})
+}
---
+++ src/routes/post.route.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const postRoute = express.Router()
+import * as postController from '../controllers/post.controller.js'
+import upload from '../middlewares/upload.middleware.js'
+
+postRoute.get('/', postController.getAllPosts)
+postRoute.post('/', upload.single('image'), postController.createPost)
+postRoute.put('/:id', postController.updatePost)
+postRoute.delete('/:id', postController.deletePost)
+
+export default postRoute
